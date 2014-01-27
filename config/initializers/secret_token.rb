3cb0dc98e2e659b4db79d39efccd6ecdddd7ec45
Luiz Picolo <luizpicolo@gmail.com>
2014-01-27 02:38:33 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebsiteRubyRspecMongodb::Application.config.secret_token = '26f5d64ccfa9b759ea2ef4d6180a440404b566276886a81d6eb5cc8c658ed9b5b7751ff0836c71cce8b7060fc0b5a1a237937b6e16a3e149d72771994d9b3db7'
