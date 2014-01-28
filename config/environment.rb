f98c441ff543dacdcbcdc0c2e990a1b8c87bf557
Luiz Picolo <luizpicolo@gmail.com>
2014-01-28 10:01:32 -0300

    Adicionado parâmetro false para desaprovações

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 WebsiteRubyRspecMongodb::Application.initialize!
 
-#ENV['RAILS_ENV'] = 'production'
+ENV['RAILS_ENV'] = 'production'
+
+if ENV['RAILS_ENV'] == "production"
+  ActiveSupport::Deprecation.silenced = true
+end
