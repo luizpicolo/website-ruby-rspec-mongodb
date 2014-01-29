2c7e897a5615351076993dcc8234bd41aca11d90
Luiz Picolo <luizpicolo@gmail.com>
2014-01-29 16:28:49 -0300

    created articles entity

---
+++ app/admin/article.rb
@@ -0,0 +1,23 @@
+ActiveAdmin.register Article do
+	index do
+		column :title
+	    column :tags
+	    column :created_at
+	    column :updated_at
+	    
+	    default_actions
+	end
+
+  filter :email
+
+	form do |f|
+		f.inputs "Articles" do
+			f.input :title
+			f.input :description
+			f.input :body, :input_html => { :class => 'autogrow', :rows => 10, :cols => 100, :maxlength => 10  }
+			f.input :position_image_highlighted, :as => :select, :collection => ["Left", "Right", "Center"]
+			f.input :tags
+		end
+		f.actions
+	end
+end
