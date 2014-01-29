5c685b7294b66f4af4a2ddf96fd9a5d49905d301
Luiz Picolo <luizpicolo@gmail.com>
2014-01-29 16:48:13 -0300

    amended type of attribute

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,7 +6,7 @@ class Article
   field :tags, type: String
   field :description, type: String
   field :body, type: String
-  field :position_image_highlighted, type: Integer
+  field :position_image_highlighted, type: String
 
   validates :title, :body, presence: true
 
