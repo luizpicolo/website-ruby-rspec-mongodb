551300ac19531eb681d4e3f7d84e721f58c9ea43
Luiz Picolo <luizpicolo@gmail.com>
2014-01-29 16:47:15 -0300

    Amended to add textarea input

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -14,8 +14,8 @@ ActiveAdmin.register Article do
 		f.inputs "Articles" do
 			f.input :title
 			f.input :description
-			f.input :body, :input_html => { :class => 'autogrow', :rows => 10, :cols => 100, :maxlength => 10  }
-			f.input :position_image_highlighted, :as => :select, :collection => ["Left", "Right", "Center"]
+			f.input :body, :input_html => { :class => 'autogrow', :rows => 10, :cols => 100, :maxlength => 10 }
+			f.input :position_image_highlighted, :as => :select, :collection => ["None", "Left", "Right", "Center"]
 			f.input :tags
 		end
 		f.actions
