bc110966ffee5be357c3174c4411648a3055e295
Luiz Picolo <luizpicolo@gmail.com>
2014-01-30 15:22:55 -0300

    Reescrito Teste

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 describe Article do
   it "object article create is valid?" do
-		article = Article.create
-		article.title = "Title for Test"
-		article.description = ""
-		article.body = "Body for Test"
-		article.position_image_highlighted = ""
+		article = Article.new({
+			title: "Title for Test",
+			description: "",
+			body: "Body for Test",
+			position_image_highlighted: ""
+		})
 
 		expect(article).to be_valid
 	end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe Tag do
   	it "object create is valid?" do
-		tag = Tag.create
-		tag.name = "Name for Tag"
+		tag = Tag.new({
+			name: "Name for Tag"
+		})
+
 		expect(tag).to be_valid
 	end
 end
