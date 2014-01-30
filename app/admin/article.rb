4cfbe2994e9e7f930533a747a68012f8097a1980
Luiz Picolo <luizpicolo@gmail.com>
2014-01-30 01:30:03 -0300

    Modificado filtros e criado o relacionamento com a entidade tags

--- app/admin/article.rb
+++ app/admin/article.rb
@@ -1,23 +1,25 @@
 ActiveAdmin.register Article do
 	index do
+		selectable_column 
 		column :title
-	    column :tags
-	    column :created_at
-	    column :updated_at
+	    column :created_at, :label => 'Create Date'
+	    column :updated_at, :label => 'Update Date'
 	    
 	    default_actions
 	end
 
-  filter :email
+	filter :title
+	filter :created_at
 
-	form do |f|
+	form :html => { :enctype => "multipart/form-data" } do |f|
 		f.inputs "Articles" do
 			f.input :title
 			f.input :description
 			f.input :body, as: :html_editor
 			f.input :position_image_highlighted, :as => :select, :collection => ["None", "Left", "Right", "Center"]
-			f.input :tags
+			f.input :tag_ids, :as => :check_boxes, :collection => Tag.asc(:name)
 		end
+
 		f.actions
 	end
 end
