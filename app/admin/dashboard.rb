3c891c666f31754644d917385f72bc9efb420aa4
Luiz Picolo <luizpicolo@gmail.com>
2014-01-31 16:13:18 -0300

    Comentada colunas da DashBoard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -14,13 +14,13 @@ ActiveAdmin.register_page "Dashboard" do
     #
     # columns do
     #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
+    #     # panel "Recent Posts" do
+    #     #   ul do
+    #     #     Article.recent(5).map do |post|
+    #     #       li link_to(post.title, admin_post_path(post))
+    #     #     end
+    #     #   end
+    #     # end
     #   end
 
     #   column do
