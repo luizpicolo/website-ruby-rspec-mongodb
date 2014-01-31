44c9883dcbe6aae484567239ab7f9f0af4f7abae
Luiz Picolo <luizpicolo@gmail.com>
2014-01-31 09:29:38 -0300

    Modificada variável para tipo de ambiente

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 WebsiteRubyRspecMongodb::Application.initialize!
 
-ENV['RAILS_ENV'] = 'production'
-
-if ENV['RAILS_ENV'] == "production"
-  ActiveSupport::Deprecation.silenced = true
-end
+ENV["RAILS_ENV"] ||= 'production'
