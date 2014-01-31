36a5236f832febde97dd561e7ae2bb54e7bc5836
Luiz Picolo <luizpicolo@gmail.com>
2014-01-31 09:35:37 -0300

    Modificada teste Tag para utilizar o FactoryGirl

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,11 +1,8 @@
 require 'spec_helper'
 
 describe Tag do
-  	it "object create is valid?" do
-		tag = Tag.new({
-			name: "Name for Tag"
-		})
-
+  	it "object tag create is valid?" do
+		tag = FactoryGirl.create(:tag)    
 		expect(tag).to be_valid
 	end
 end
