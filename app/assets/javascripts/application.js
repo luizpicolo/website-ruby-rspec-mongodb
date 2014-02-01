aad096d63dd54f4f9cc91ac3a409dca7e75a2bcd
Luiz Picolo <luizpicolo@gmail.com>
2014-01-31 21:37:21 -0300

    Adicionado código JS para menu selecionado

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,1 @@
-/*
-	Code JavaScript
-*/
-
-$(document).ready(function() {
- 	//alert('Thanks for visiting!');
-});
+var url=window.location,urlString=url.toString(),urlArray=urlString.split("/");menu(urlArray[3]);function menu(a){$("#nav-principal a").each(function(){a&&($(this).parent().removeClass(),$(this).attr("href")==a&&$(this).parent().attr("class","selecionado"))})}$(document).scroll(function(){140<=parseInt($(this).scrollTop())?$("#nav-principal").css({position:"fixed",top:"-15px",background:"#FFF",width:"780px"}):$("#nav-principal").css({position:"relative",top:"0"})});
