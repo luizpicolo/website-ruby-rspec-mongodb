d6f4a0f613796d07410f9fb9fb0634c0ff90acd1
Luiz Picolo <luizpicolo@gmail.com>
2014-02-03 22:12:52 -0300

    Criado teste para retorno do metodo index

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,8 +4,18 @@ describe HomeController do
 
   describe "GET 'index'" do
     it "returns http success" do
-      get 'index'
-      expect(response).to be_success
+		get 'index'
+      	expect(response).to be_success
+    end
+
+    it "return is of type article object" do
+		get 'index'
+		expect(assigns(:articles)).to have_at_least(1).articles
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
     end
   end
 
