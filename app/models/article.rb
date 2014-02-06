c3ee25431b300926b69c763b373e1a6a312c0db6
Luiz Picolo <luizpicolo@gmail.com>
2014-02-05 22:57:51 -0300

    Adicionado atributo Slong (Pretty URL)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,13 @@
 class Article
 	include Mongoid::Document
 	include Mongoid::Timestamps
+	include Mongoid::Slug
 
-	field :title, type: String
-	field :description, type: String
-	field :body, type: String
-	field :position_image_highlighted, type: String
+	field 	:title, type: String
+	field 	:description, type: String
+	field 	:body, type: String
+	field 	:position_image_highlighted, type: String
+	slug 	:title, history: true
 
 	has_and_belongs_to_many :tags
 
