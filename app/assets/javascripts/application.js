a26c19c7fe02177cd66e20f79dfb1deb875aa076
Luiz Picolo <luizpicolo@gmail.com>
2014-02-06 07:26:02 -0300

    Modificações antias

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,12 @@
 /*
 	Code JavaScript
 */
+function menu(){
+	$("#nav-principal a").each(function(){
+		alert($(this).attr("href"));
+		$(this).attr("href") == window.location.pathname && $(this).parent().attr("class","selecionado")
+	})
+};
+menu();
 
-$(document).ready(function() {
- 	//alert('Thanks for visiting!');
-});
+$(document).scroll(function(){140<=parseInt($(this).scrollTop())?$("#nav-principal").css({position:"fixed",top:"-15px",background:"#FFF",width:"780px"}):$("#nav-principal").css({position:"relative",top:"0"})});
