b53f96a7a27548ce3239c97037312d25c2740876
Luiz Picolo <luizpicolo@gmail.com>
2014-02-07 23:35:41 -0300

    Adicionado atributo para imagem em destaque

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,6 +7,7 @@ class Article
 	field 	:description, type: String
 	field 	:body, type: String
 	field 	:position_image_highlighted, type: String
+	field	:image_highlighted, type: String
 	slug 	:title, history: true
 
 	has_and_belongs_to_many :tags
