0f1e9c241f8bafff528b14ec6330ee16160f1f4b
Luiz Picolo <luizpicolo@gmail.com>
2014-02-14 13:49:10 -0300

    Modificações de estilização

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,2 @@
+//= require ckeditor/init
 //= require active_admin/base
--- app/models/article.rb
+++ app/models/article.rb
@@ -7,9 +7,11 @@ class Article
 	field 	:description, type: String
 	field 	:body, type: String
 	field 	:position_image_highlighted, type: String
+	field	:image_highlighted, type: String
+	field	:figure_caption, type: String
 	slug 	:title, history: true
 
 	has_and_belongs_to_many :tags
 
-	validates :title, :body, presence: true
+	validates :title, :body, :position_image_highlighted, presence: true
 end
