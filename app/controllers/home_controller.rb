0233c4205db0f9a9529a4bc3c02f7bbc75374e4e
Luiz Picolo <luizpicolo@gmail.com>
2014-02-15 22:36:14 -0300

    Modificado retorno dos arigos para ordem decrescente

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	def index
-		@articles = Article.all
+		@articles = Article.order_by(:published_in.desc)
 		@title = "Luiz Picolo - Desenvolvimento Web"
 		@description = "Sou licenciado em História pela Universidade Federal de Mato Grosso do Sul - UFMS (2011) / Campus de Nova Andradina - CPNA. Atualmente sou graduando em Tecnologia em Análise e Desenvolvimento de Sistemas pelo Instituto Federal de Educação, Ciência e Tecnologia de Mato Grosso do Sul - TADS - IFMS (2012) / Campus de Nova Andradina."
 	end
