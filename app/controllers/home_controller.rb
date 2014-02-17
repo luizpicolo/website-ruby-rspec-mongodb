77d67784c5250e880e8b5090e1821e3fe87f5453
Luiz Picolo <luizpicolo@gmail.com>
2014-02-17 10:57:06 -0400

    Adicionado retorno dos slides

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
 		@articles = Article.order_by(:published_in.desc)
 		@title = "Luiz Picolo - Desenvolvimento Web"
 		@description = "Sou licenciado em História pela Universidade Federal de Mato Grosso do Sul - UFMS (2011) / Campus de Nova Andradina - CPNA. Atualmente sou graduando em Tecnologia em Análise e Desenvolvimento de Sistemas pelo Instituto Federal de Educação, Ciência e Tecnologia de Mato Grosso do Sul - TADS - IFMS (2012) / Campus de Nova Andradina."
+		@slides = Slide.all();
 	end
 end
