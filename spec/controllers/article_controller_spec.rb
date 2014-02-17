7f0a9bc1f1e4b700eda3088b33c11ca3746bc240
Luiz Picolo <luizpicolo@gmail.com>
2014-02-17 09:54:01 -0400

    Modificado Spec para atender as necessidades do teste

--- spec/controllers/article_controller_spec.rb
+++ spec/controllers/article_controller_spec.rb
@@ -4,13 +4,13 @@ describe ArticleController do
 
   describe "GET show" do
   	it "return one article valid?" do
-  		get :show
-  		expect(assigns(:article)).to eq(Article.new.title)
-  		expect(assigns(:article)).to eq(Article.new.body)
+  		get :show, {:title => "title-for-test"}
+  		expect(assigns(:article).title).to eq("Title for Test")
+  		expect(assigns(:article).body).to eq("Body for Test")
   	end
 
   	it "renders the show template" do
-      get :show
+      get :show, {:title => "title-for-test"}
       expect(response).to render_template("show")
     end
   end
