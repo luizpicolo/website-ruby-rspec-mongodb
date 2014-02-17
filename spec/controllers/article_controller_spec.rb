d70ab7ba7b496bacd1eaabf5031706ce6e389d34
Luiz Picolo <luizpicolo@gmail.com>
2014-02-17 10:57:57 -0400

    Adicionado correção para teste do controle article

--- spec/controllers/article_controller_spec.rb
+++ spec/controllers/article_controller_spec.rb
@@ -5,12 +5,14 @@ describe ArticleController do
   describe "GET show" do
   	it "return one article valid?" do
   		get :show, {:title => "title-for-test"}
-  		expect(assigns(:article).title).to eq("Title for Test")
+  		
+      expect(assigns(:article).title).to eq("Title for Test")
   		expect(assigns(:article).body).to eq("Body for Test")
   	end
 
   	it "renders the show template" do
       get :show, {:title => "title-for-test"}
+      
       expect(response).to render_template("show")
     end
   end
