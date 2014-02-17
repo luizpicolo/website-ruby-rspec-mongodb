c1b8b63f7643c699171404467d0a5fbd2bf4f0a6
Luiz Picolo <luizpicolo@gmail.com>
2014-02-17 15:19:01 -0400

    Adicionada validações

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,6 +1,10 @@
 class Slide
   include Mongoid::Document
+  include Mongoid::Timestamps
+
   field :title, type: String
   field :link, type: String
   field :image, type: String
+
+  validates :title, :image, presence: true
 end
