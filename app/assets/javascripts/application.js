e0edf0de3a40430a4d1245ff62a0d6a210db70f2
Luiz Picolo <luizpicolo@gmail.com>
2014-02-17 21:15:46 -0400

    Modificado menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,14 +13,14 @@ $(document).ready(function() {
 	});
 });
 
-function menu(){
-	$("#nav-principal a").each(function(){
-		alert($(this).attr("href"));
-		$(this).attr("href") == window.location.pathname && $(this).parent().attr("class","selecionado")
-	})
-};
+// function menu(){
+// 	$("#nav-principal a").each(function(){
+// 		alert($(this).attr("href"));
+// 		$(this).attr("href") == window.location.pathname && $(this).parent().attr("class","selecionado")
+// 	})
+// };
 
-menu();
+// menu();
 
-$(document).scroll(function(){140<=parseInt($(this).scrollTop())?$("#nav-principal").css({position:"fixed",top:"-15px",background:"#FFF",width:"780px"}):$("#nav-principal").css({position:"relative",top:"0"})});
-var url=window.location,urlString=url.toString(),urlArray=urlString.split("/");menu(urlArray[3]);function menu(a){$("#nav-principal a").each(function(){a&&($(this).parent().removeClass(),$(this).attr("href")==a&&$(this).parent().attr("class","selecionado"))})}$(document).scroll(function(){140<=parseInt($(this).scrollTop())?$("#nav-principal").css({position:"fixed",top:"-15px",background:"#FFF",width:"780px"}):$("#nav-principal").css({position:"relative",top:"0"})});
+// $(document).scroll(function(){140<=parseInt($(this).scrollTop())?$("#nav-principal").css({position:"fixed",top:"-15px",background:"#FFF",width:"780px"}):$("#nav-principal").css({position:"relative",top:"0"})});
+// var url=window.location,urlString=url.toString(),urlArray=urlString.split("/");menu(urlArray[3]);function menu(a){$("#nav-principal a").each(function(){a&&($(this).parent().removeClass(),$(this).attr("href")==a&&$(this).parent().attr("class","selecionado"))})}$(document).scroll(function(){140<=parseInt($(this).scrollTop())?$("#nav-principal").css({position:"fixed",top:"-15px",background:"#FFF",width:"780px"}):$("#nav-principal").css({position:"relative",top:"0"})});
