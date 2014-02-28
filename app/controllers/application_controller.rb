eaed73f8ab54ed9f25f7e29c60a93c07a337a9ba
Luiz Picolo <luizpicolo@gmail.com>
2014-02-28 13:40:10 -0400

    Retirada codigo para o redirecionamento da página de erros

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,3 @@
 class ApplicationController < ActionController::Base
-	protect_from_forgery
-	
-	rescue_from ActionController::RoutingError, :with => :route_not_found
-	rescue_from ActionController::MethodNotAllowed, :with => :invalid_method
-
-	def route_not_found
-		render :text => 'What the fuck are you looking for ?', :status => :not_found
-	end
-
-	def invalid_method
-		message = "Now, did your mom tell you to #{request.request_method.to_s.upcase} that ?"
-		render :text => message, :status => :method_not_allowed
-	end
-
+  protect_from_forgery
 end
