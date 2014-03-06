b57935a71e898414923ddcde78d6d9141daba98c
Luiz Picolo <luizpicolo@gmail.com>
2014-03-06 09:24:27 -0400

    Adicionado novos paramentros para simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'rspec/autorun'
 require 'factory_girl_rails'
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start 'rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
