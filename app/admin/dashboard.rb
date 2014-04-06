2e3015e16744c983dedd83bdbd0815d6614f400d
Luiz Picolo <luizpicolo@gmail.com>
2014-04-06 01:33:01 -0400

    Removido gráficos da DashBoard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,37 +3,6 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    # div :class => "blank_slate_container", :id => "dashboard_default_message" do
-    #   span :class => "blank_slate" do
-    #     span I18n.t("active_admin.dashboard_welcome.welcome")
-    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
-    #   end
-    # end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-
-    charts = ChartController.new
-
-    columns do
-      column do
-        panel "Articles by tags" do
-            para bar_chart charts.articles_by_tags
-        end
-      end
-
-      column do
-        panel "Articles by Date" do
-            #para line_chart charts.articles_by_date
-        end
-      end
-
-      column do
-        panel "Number of articles" do
-          para "Articles Published:  #{Article.where(:status => 'published').count}"
-          para "Articles Unpublished:  #{Article.where(:status => 'unpublished').count}"
-        end
-      end
-    end
-  end # content
+    
+  end 
 end
